856695562d5511f4e7d9c147a5bd1df8dddadeba
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-13 18:05:08 +0100

    redux counter works

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as creators from '../state/actionCreators';
+
+export function Counter (props) {
+
+
+    const { count, increment, decrement, reset} = props;
+    return (
+        <div>
+            The count is {count}
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        count: state.count,
+    }
+}
+
+export default connect(mapStateToProps,
+    creators,
+    )(Counter);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './state/reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    {},
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),   
+)
+
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
